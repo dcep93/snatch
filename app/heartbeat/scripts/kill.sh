908eaff0863b1e91507d3253444758fe24bf1e0e
dcep93 <dan@quizlet.com>
2020-03-22 18:45:59 -0700

    need to use the var...

--- app/heartbeat/scripts/kill.sh
+++ app/heartbeat/scripts/kill.sh
@@ -4,7 +4,7 @@ exec &> >(tee -a /var/log/snatch.log)
 
 MACHINE=$(cat /var/log/cheat_machine.txt)
 echo "$(date) $(whoami) $MACHINE kill"
-gcloud compute instances stop instance-1
+gcloud compute instances stop "$MACHINE"
 code=$?
 echo "$(date) $code killed"
 echo
